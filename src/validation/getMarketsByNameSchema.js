b9f2e0385bf5a12a0b11b01a4359d7110bcf4bcb
Babatunde <soft-sky@live.co.uk>
2020-06-01 05:39:48 +0100

    chore[refactor-getMarketsByName]: Reafactor get markets by name endpoint
    - [x] Modify market controller
    - [x] Modify route handler
    - [x] Modify validation schema
    
    [#173101132]

--- src/validation/getMarketsByNameSchema.js
+++ src/validation/getMarketsByNameSchema.js
@@ -1,9 +1,9 @@
-import { check } from 'express-validator';
+import { query } from 'express-validator';
 import capitalize from '../utils/capitalize';
 
 /* eslint-disable arrow-parens */
 const schema = [
-    check('name')
+    query('name')
         .trim()
         .exists().withMessage('name is required')
         .isLength({ min: 2, max: 100 }).withMessage('name is required and must not be less than two characters')
